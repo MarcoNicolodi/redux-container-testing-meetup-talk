7b7ce405e2948c3c2076c74a05027b4a7a0a3755
MarcoNicolodi <marcocontatopro@gmail.com>
2018-04-22 14:32:41 -0300

    add cypress e2e testes

---
+++ cypress/integration/list_spec.js
@@ -0,0 +1,28 @@
+describe("first test", () => {
+  it("GIVEN that im on listing page, WHEN i visit the page, THEN i should see first page documents", () => {
+    cy.visit("localhost:8080");
+    // assert input is in page 1
+    cy.get("li.rc-pagination-simple-pager input").should("have.value", "1");
+    cy.contains("Document list");
+    cy.contains("PO0003");
+  });
+
+  it("GIVEN that im on listin page, WHEN i click the next button, THEN i should see second page documents", () => {
+    cy.visit("localhost:8080");
+    cy.get("table");
+    cy.get("button#pagination-next-button").click();
+    cy.get("li.rc-pagination-simple-pager input").should("have.value", "2");
+    cy.get("table").contains("FP0034");
+  });
+
+  it("GIVEN that im on listing page, WHEN i fill the page input with page 3 and press enter, THEN i should see third page documents", () => {
+    cy.visit("localhost:8080");
+    cy.get("table");
+    cy
+      .get("li.rc-pagination-simple-pager input")
+      .type("{backspace}")
+      .type("3")
+      .type("{enter}")
+      .should("have.value", "3");
+  });
+});
