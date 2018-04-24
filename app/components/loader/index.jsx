6e49eae66ba4d594a8e2e9d425833d5810878a5b
MarcoNicolodi <marcocontatopro@gmail.com>
2018-04-24 20:50:13 -0300

    new loader

--- app/components/loader/index.jsx
+++ app/components/loader/index.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React from "react";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faSpinner from "@fortawesome/fontawesome-free-solid/faSpinner";
 
-const Loader = () => <span> loading... </span>;
+const Loader = () => (
+  <span>
+    <FontAwesomeIcon icon={faSpinner} spin size="lg" />
+  </span>
+);
 
 export default Loader;
-
--- app/features/list/api/apiClient.js
+++ app/features/list/api/apiClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const options = {
   headers: {
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTEyIiwibG9nZ2VkX2NvbXBhbnkiOiI3IiwiY29tcGFueV9kb21haW4iOiJxdWFseXRlYW0tb2ZpY2lhbCIsImV4cCI6MTUyNDUwMTAzOSwiaXNzIjoiaHR0cDovLzEyNy4wLjAuMS8iLCJhdWQiOiJodHRwOi8vMTI3LjAuMC4xLyJ9.MZcio99ezKA2xGVdvGM_5MctXxmQNoK7eXQvWM6ZdjY"
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTEyIiwibG9nZ2VkX2NvbXBhbnkiOiI3IiwiY29tcGFueV9kb21haW4iOiJxdWFseXRlYW0tb2ZpY2lhbCIsImV4cCI6MTUyNDY5ODA1NywiaXNzIjoiaHR0cDovLzEyNy4wLjAuMS8iLCJhdWQiOiJodHRwOi8vMTI3LjAuMC4xLyJ9.oIfZHjyX8ECjcAvYGJ5vLmH4Utz1m28U9D40GEHN3Fo"
   }
 };
 
